143a0c76e664fc5aea36641ef940b5a82abf2f29
Fernando Camacho <fernando.camacho@peopleart.co>
2024-11-28 11:24:27 +1100

    Initial commit

---
+++ capacitor.config.ts
@@ -0,0 +1,9 @@
+import type { CapacitorConfig } from '@capacitor/cli';
+
+const config: CapacitorConfig = {
+  appId: 'co.peopleart.gastos',
+  appName: 'gastos',
+  webDir: 'www'
+};
+
+export default config;
